9cc306c621d276520bf09d3982a408d72f38d56d
Md. Neyamul Islam <158471735+NayamulNirob@users.noreply.github.com>
2024-09-07 19:05:17 +0600

    FromTsp 7:04PM
    07/09/2024

---
+++ merchandiseMgmtERPSpringBoot/src/main/java/com/merchandisemgmt/merchandiseMgmtERP/entity/Product.java
@@ -0,0 +1,26 @@
+package com.merchandisemgmt.merchandiseMgmtERP.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+    private String description;
+    private Long price;
+    private Long quantity;
+
+
+}

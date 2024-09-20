5cacd1b4b30666bcf458693b1d8293000b9f9077
NayamulNirob <158471735+NayamulNirob@users.noreply.github.com>
2024-09-21 04:42:57 +0600

    Customer Remove 4:42AM
    21/09/2024

--- merchandiseMgmtERPSpringBoot/src/main/java/com/merchandisemgmt/merchandiseMgmtERP/entity/Customer.java
+++ merchandiseMgmtERPSpringBoot/src/main/java/com/merchandisemgmt/merchandiseMgmtERP/entity/Customer.java
@@ -38,4 +38,5 @@ public class Customer {
     private LocalDateTime updatedAt;
 
 
+
 }
--- merchandiseMgmtERPSpringBoot/src/main/java/com/merchandisemgmt/merchandiseMgmtERP/repository/SaleRepository.java
+++ merchandiseMgmtERPSpringBoot/src/main/java/com/merchandisemgmt/merchandiseMgmtERP/repository/SaleRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SaleRepository extends JpaRepository<Sale, Long> {
+    long countByCustomerId(Long id);
 }
